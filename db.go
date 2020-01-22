0a09af25e4455082ae25115e74412c6db3c1d5c6
Manikandan <manikandan0603@gmail.com>
2020-01-22 18:06:57 +0530

    fix: maxmind geolite ur lreplaecs with custom variable

--- db.go
+++ db.go
@@ -35,7 +35,7 @@ var (
 	defaultDB = filepath.Join(os.TempDir(), "freegeoip", "db.gz")
 
 	// MaxMindDB is the URL of the free MaxMind GeoLite2 database.
-	MaxMindDB = "http://geolite.maxmind.com/download/geoip/database/GeoLite2-City.mmdb.gz"
+	MaxMindDB = os.Getenv("GEOLITE2_DB_URL")
 )
 
 // DB is the IP geolocation database.
